2628418a527547dfdbd280637174e0876be3d6b8
Jeffrey Sambells <jeffrey@speakfeel.ca>
2015-12-11 20:19:40 -0500

    Added alc

---
+++ alc.rb
@@ -0,0 +1,14 @@
+class Alc < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/iamamused/alc/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "7383e775aec3300e990dbba2a96a29372370e52952047cca88eda3285a05780a"
+
+ # depends_on :cask alcatraz
+
+  def install
+	bin.install "alc"
+  end
+
+end
