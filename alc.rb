b134c213614180843a78a7251110122eb86d25a9
Jeffrey Sambells <jeffrey@speakfeel.ca>
2015-12-15 23:59:12 -0500

    removed depends

--- alc.rb
+++ alc.rb
@@ -5,7 +5,7 @@ class Alc < Formula
   version "1.0.0"
   sha256 "7383e775aec3300e990dbba2a96a29372370e52952047cca88eda3285a05780a"
 
-  depends_on jq
+ # depends_on jq
 
   def install
 	bin.install "alc"
