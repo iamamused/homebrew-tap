c17845cc62af9921b174c97ece54fe092d3da4c3
Jeffrey Sambells <jeffrey@speakfeel.ca>
2016-01-22 22:23:39 -0500

    updated libjsambells

--- libjsambells.rb
+++ libjsambells.rb
@@ -1,17 +1,22 @@
 # Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+#
+# brew edit libjsambells
+# update the version number
+# brew fetch libjsambells --build-from-source
+# copy the hash
 
 class Libjsambells < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/iamamused/libjsambells/archive/1.0.3.tar.gz"
-  version "1.0.3"
-  sha256 "1666374362e88cc3092e7ff2d95824e0341070557ccacc55f1e25a9214e4df98"
+  desc "Collection of tools freely available online elsewhere"
+  homepage "http://jeffreysambells.com"
+  url "https://github.com/iamamused/libjsambells/archive/1.0.4.tar.gz"
+  version "1.0.4"
+  sha256 "25b355350e4bdc0219b7e0a416baf51a2c4f5093794d9c4894139a43d779f07e"
 
   def install
 	  system "make", "-f", "BrewMakefile"
 	  share.install Dir["./share/*"]
 	  bin.install "bin/googlesheet2localizablestrings"
   end
+  
 end
