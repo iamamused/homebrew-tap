8ffa752f0840b35da4ebebad9a2bebd231bbcce8
Jeffrey Sambells <jeffrey@speakfeel.ca>
2016-01-27 23:45:25 -0500

    update libjsambells tap.

--- libjsambells.rb
+++ libjsambells.rb
@@ -5,13 +5,16 @@
 # update the version number
 # brew fetch libjsambells --build-from-source
 # copy the hash
+# push to git
+# brew update
+# brew upgrade libjsambells
 
 class Libjsambells < Formula
   desc "Collection of tools freely available online elsewhere"
   homepage "http://jeffreysambells.com"
   url "https://github.com/iamamused/libjsambells/archive/1.0.6.tar.gz"
   version "1.0.6"
-  sha256 "4a06cc2a502a36f0c1bbf8e82cc6a16a3df0d9cffe79da71b88e410dae38e47b"
+  sha256 "0310b75b7dba116eb4f8824df92ba68718aad9cef5afb5db4543477cf5c493f3"
 
   def install
 	  system "make", "-f", "BrewMakefile"
